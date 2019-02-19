1d2a67a3af22d984b69ab0993a894a7482a24a64
Protor_fenix <Protor_fenix@mail.ru>
2019-02-19 14:05:32 +0300

    Задача 4

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,56 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+
+import random
+
+x = input('Укажите нижнюю границу: ')
+y = input('Укажите верхнюю границу: ')
+
+try:
+    # пробуем целый числа
+    xx = float(x)
+    yy = float(y)
+    result = random.uniform(xx, yy)
+    if round(xx, 0) == xx and round(yy, 0) == yy:
+        result = int(result)
+    else:
+        # определяем количество знаков после запятой
+        if x.find('.') > 0:  # проверка на наличие точки
+            split_x = x.split('.')
+            len_x = len(split_x[1])
+        else:
+            len_x = 1
+
+        if y.find('.') > 0:  # проверка на наличие точки
+            split_y = y.split('.')
+            len_y = len(split_y[1])
+        else:
+            len_y = 1
+
+        # выбираем максимально длинную дробную часть
+        if len_x >= len_x:
+            len_max = len_x
+        else:
+            len_max = len_y
+
+        result = round(result, len_max)
+
+    print('целые', result)
+
+
+except Exception:
+    try:
+        # пробуем текст
+        xx = ord(x)
+        yy = ord(y)
+        print(xx, yy)
+
+        result = random.randint(xx, yy)
+
+        result = chr(result)
+
+        print(result)
+
+    except Exception:
+        print('Не корректный ввод')
