2768149e62c1b2e893d592d74c5ac3190e97f561
Protor_fenix <Protor_fenix@mail.ru>
2019-03-08 11:46:53 +0300

    ДЗ 5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,42 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+import collections
+
+# условный ввод
+factory = ['кондитерская', 'хлебопекарня', 'шиномонтаж']
+profit_1 = [10, 9, 11, 13]
+profit_2 = [9, 8, 5, 13]
+profit_3 = [11, 13, 14, 16]
+
+profit = list((profit_1, profit_2, profit_3))
+base = collections.Counter()
+average_annual_profit = collections.Counter()
+
+# формируем базу
+for i in range(len(factory)):
+    base[factory[i]] = profit[i]
+
+print(base)
+
+
+def mean(nums):
+    """ возвращает среднее значение """
+    return sum(nums, 0.0) / len(nums)
+
+
+# определяем среднюю годовую прибыль каждого предприятия
+for elm in factory:
+     average_annual_profit[elm] = mean(base[elm])
+
+print(average_annual_profit)
+
+# предприятия с прибылью выше среднего.
+max_profit = max(average_annual_profit, key=lambda k: average_annual_profit[k])
+
+# предприятия с наименьшей прибылью.
+min_profit = min(average_annual_profit, key=lambda k: average_annual_profit[k])
+
+# вывод
+print(f'наибольшая прибыль у {max_profit} = {average_annual_profit[max_profit]},'
+      f' наименьшая прибыль у {min_profit} = {average_annual_profit[min_profit]}')
